a74f76d181b118afacfb6bdf6afe621d9b32216a
alishabhaisare12 <alishabhaisare12@gmail.com>
2026-03-19 09:15:23 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,17 @@
+class Person {
+    String name;
+    int age;
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+    public static void main(String[] args) {
+        Person p1 = new Person("Alice", 25);
+        Person p2 = new Person("Bob", 30);
+        p1.display();
+        p2.display();
+    }
+}
